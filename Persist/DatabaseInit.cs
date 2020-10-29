e502875a7838a30976e4b7102bbbbf1f4258eb01
Алекс <aleks.andr.98@outlook.com>
2020-10-29 18:23:24 +0300

    Доработка

--- Persist/DatabaseInit.cs
+++ Persist/DatabaseInit.cs
@@ -44,7 +44,7 @@ namespace Persist
                     User = user
                 };
                     
-                await _userManager.CreateAsync(user, "1234"); 
+                var result = await _userManager.CreateAsync(user, "!123zZ"); 
 
                 try
                 {  
